8911839c8591980fa22b787063db616c2b9908fd
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-05 22:40:28 +0530

    Added more stories and choices

--- DestiniApp/Model/Story.swift
+++ DestiniApp/Model/Story.swift
@@ -8,7 +8,17 @@
 import Foundation
 
 struct Story{
-    let storyTitle: String
-    let firstChoiceValue: String
-    let secondChoiceValue: String
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int){
+        self.title = title
+        self.choice1 = choice1
+        self.choice1Destination = choice1Destination
+        self.choice2 = choice2
+        self.choice2Destination = choice2Destination
+    }
 }
